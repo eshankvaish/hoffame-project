658dc35625e3e798c498c959853215360126c6dc
Eshank Vaish <eshank.vaish@gmail.com>
2020-04-08 03:41:11 +0530

    upto Youtube API completed

---
+++ halls/views.py
@@ -0,0 +1,97 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from .models import Hall, Video
+from django.contrib.auth import authenticate, login
+from .forms import VideoForm, SearchForm
+from django.http import Http404
+import urllib
+from django.forms.utils import ErrorList
+import requests
+
+#from django.forms import formset_factory
+
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'halls/home.html')
+
+def dashboard(request):
+    return render(request, 'halls/dashboard.html')
+
+def add_video(request, pk):
+    #VideoFormSet = formset_factory(VideoForm, extra=5)
+    #form = VideoFormSet()
+    form = VideoForm()
+    search_form = SearchForm()
+    hall = Hall.objects.get(pk=pk)
+    if not hall.user == request.user:
+        raise Http404
+
+    if request.method == 'POST':
+        form = VideoForm(request.POST)
+        if form.is_valid():
+            video = Video()
+            video.hall = hall
+            video.url = form.cleaned_data['url']
+            parsed_url = urllib.parse.urlparse(video.url)
+            video_id = urllib.parse.parse_qs(parsed_url.query).get('v')
+            if video_id:
+                video.youtube_id = video_id[0]
+                response = requests.get(f'https://www.googleapis.com/youtube/v3/videos?part=snippet&id={ video_id[0] }&key={ YOUTUBE_API_KEY }').json()
+                title = response['items'][0]['snippet']['title']
+                #print(title)
+                video.title = title
+                video.save( )
+
+                return redirect('detail_hall', pk)
+            else:
+                errors = form._errors.setdefault('url', ErrorList())
+                errors.append("Needs to be a valid youtube URL")
+
+    return render(request, 'halls/add_video.html', {'form':form, 'search_form': search_form, 'hall': hall, })
+
+class SignUp(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
+# def create_hall(request):
+    
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
+
+class DetailHall(generic.DetailView):
+    model = Hall
+    template_name = 'halls/detail_hall.html'
+
+
+class UpdateHall(generic.UpdateView):
+    model = Hall
+    template_name = 'halls/update_hall.html'
+    fields = ['title']
+    success_url = reverse_lazy('dashboard')
+    #success_url = redirect('halloffame'+str(.id))
+
+
+class DeleteHall(generic.DeleteView):
+    model = Hall
+    template_name = 'halls/delete_hall.html'
+    success_url = reverse_lazy('dashboard')
